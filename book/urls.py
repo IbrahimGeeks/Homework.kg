4cb8cb9dedff7aa4ca5571dd88104dbea9cb4840
Ibrahim_geeks <fourdeltaone100@gmail.com>
2026-04-30 19:51:53 +0600

    дополнил дз

--- book/urls.py
+++ book/urls.py
@@ -5,4 +5,6 @@ urlpatterns = [
     path('message1/', views.message1, name='message1'),
     path('message2/', views.message2, name='message2'),
     path('message3/', views.message3, name='message3'),
+    path('', views.book_list_view, name='book_list'),
+    path('book_detail/<int:id>/', views.book_detail_view, name='book_detail'),
 ]
